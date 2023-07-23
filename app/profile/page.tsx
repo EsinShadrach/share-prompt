dc4f06d5ae1fd69edad3fda815afd6eb412f68a8
EsinShadrach <esinshadrachrafe@gmail.com>
2023-07-23 20:09:25 +0100

    done

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -27,8 +27,26 @@ export default function Page() {
 	const handleEdit = (post: Post) => {
 		router.push(`/update-prompt?_id=${post._id}`);
 	};
-	const handleDelete = (post: Post) => {
-		router.push(`/delete-prompt?_id=${post._id}`);
+	const handleDelete = async (post: Post) => {
+		const hasConfirmed = confirm(
+			"Are you sure you want to delete this post?"
+		);
+		
+		if (hasConfirmed) {
+			try {
+				await fetch(`/api/prompts/${post._id.toString()}`, {
+					method: "DELETE",
+				});
+
+				const filteredPost = posts.filter(
+					(post) => post._id !== post._id
+				);
+				
+				setPosts(filteredPost);
+			} catch (error) {
+				console.error(error);
+			}
+		}
 	};
 
 	return (
